58df46539daf4ef1d4db5735f2da04b47c6aca54
Leonardo-Boss <70913810+Leonardo-Boss@users.noreply.github.com>
2022-09-15 00:04:01 -0300

    add e1d3.c

---
+++ e1d3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int
+main()
+{
+    float fahr, celsius;
+    int begin, end, incr;
+
+    begin = 0;
+    end = 300;
+    incr = 20;
+
+     fahr = begin;
+     printf("fahr celsius\n");
+     while (fahr <= end) {
+        celsius = (5.0/9.0) * (fahr - 32.0);
+        printf("%4.0f %6.1f\n", fahr, celsius);
+        fahr = fahr + incr;
+     }
+}
